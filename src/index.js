5ec47a3851da6d2d945adb94d710c916f5a128d0
shiv chhiroliya <chhiroliyashiv2009@gmail.com>
2026-06-30 15:25:52 +0530

    db connection

---
+++ src/index.js
@@ -0,0 +1,50 @@
+require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/i.js";
+
+// dotenv.config({
+//     path:'./'
+// })
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express()
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("err :", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("error :", error);
+    throw error;
+  }
+})();
+*/
